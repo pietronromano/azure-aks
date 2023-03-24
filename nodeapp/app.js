f29f59ebb2b22947423b7e5ed9434ee500ae3d31
Pietro N. Romano <pietronromano@live.com>
2023-03-24 18:51:49 +0100

    Added Probe code in app.js, Samsung

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -3,35 +3,57 @@ const  os = require('os');
 const fs = require('fs');
 
 
-const podApp = process.env.APPNAME || process.argv[2]; 
+const podApp = process.env.APPNAME || process.argv[2] || "App Name not supplied in APPNAME nor argv[2] "; 
 const port = process.env.PORT || 8080;
 
 console.log("Pod App: " + podApp + " starting... Listening on port " + port);
 
-var handler = function(request, response) {
-  var msg = {
+const getRequestInfo = function(req) {
+  var info = {
     "podApp" : podApp,
     "podHostName": os.hostname(),
-    "remoteAddress" : "Request received from: "  + request.connection.remoteAddress 
-                      + ":" + request.connection.remotePort,
-    "requestHostHeader" : request.headers["host"],
+    "remoteAddress" : "Request received from: "  + req.connection.remoteAddress 
+                      + ":" + req.connection.remotePort,
+    "requestHostHeader" : req.headers["host"],
     "time" : new Date().toUTCString()
   }
-  var strMsg = JSON.stringify(msg);
-  console.dir(msg);
-  response.writeHead(200);
-  var logFile = './logs/applogs.txt'; //Source code root directory  - works fine
-  fs.open(logFile, 'a', function(err, fd) {
+  var strInfo = JSON.stringify(info);
+  console.dir(info);
+  var logFile = '/applogs.txt'; 
+  var logDir = "./logs";
+  var filePath = logDir + logFile;
+  if(!fs.existsSync(logDir))
+    fs.mkdirSync(logDir)
+  fs.open(filePath, 'a', function(err, fd) {
     if (err) {
        console.log(err);
     }
-    fs.appendFile(logFile, strMsg +  "\n", function (err) {
+    fs.appendFile(filePath, strInfo +  "\n", function (err) {
       if (err) 
         console.log(err);
     });
   });
-  response.end(strMsg);
+  //Return the  info
+   return strInfo;
 };
 
-var www = http.createServer(handler);
+const requestHandler = function (req, res) {
+  res.setHeader("Content-Type", "application/json");
+  switch (req.url) {
+      case "/info":
+          res.writeHead(200);
+          var strInfo = getRequestInfo(req);
+          res.end(strInfo); //must be a string
+          break
+      case "/probe":
+          res.writeHead(200);
+          res.end(JSON.stringify({probeStatus:"alive"}));
+          break
+      default:
+          res.writeHead(404);
+          res.end(JSON.stringify({error:"Resource not found"}));
+  }
+}
+
+var www = http.createServer(requestHandler);
 www.listen(port);
